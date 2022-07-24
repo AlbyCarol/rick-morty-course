704d81366fd57578ce7ca5df6ee978cc26b0b50d
AlbyCarol <albinys_suarez@hotmail.com>
2022-07-24 12:39:44 -0400

    first commit course

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import imagenRickMorty from './img/rick-morty.png';
+import './App.css';
+import { useState } from 'react';
+import Characters from './components/Characters';
+
+function App() {
+ const [characters, setCharacters]= useState(null);
+  const recApi = async() =>{
+    const api= await fetch('https://rickandmortyapi.com/api/character');
+    const characterApi= await api.json();
+    setCharacters(characterApi.results)
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1 className='tittle'>Rick & Morty</h1>
+       <h4 className='sub-tittle'>by Albinys Suarez</h4>
+       {characters ? (
+        <Characters characters ={characters} setCharacters= {setCharacters}/>
+       ) : (
+        <>
+        <img src={imagenRickMorty} alt="Rick & Morty" className='img/home' />
+       <button onClick={recApi} className="btn-search">Buscar Personajes</button>
+       </>
+       )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
